eea77b03bb8bdd7b6cc895b01eb3522cb33214a5
praczu99 <72567448+praczu99@users.noreply.github.com>
2020-11-10 09:40:55 +0100

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,21 @@
+let button_1 = document.querySelector('.scroll');
+
+
+button_1.addEventListener('click', goToTop);
+
+
+function goToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+
+function showTopButton() {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    	document.querySelector(".scroll").style.display = "block";
+	} else {
+    	document.querySelector(".scroll").style.display = "none";
+	}
+}
+
+window.onscroll = function() {showTopButton()};
